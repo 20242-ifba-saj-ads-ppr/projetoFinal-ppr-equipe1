38962ea748ca176132837a856d9b4d9b24c62b26
kaiostavares <kaiosandek@outlook.com>
2025-03-26 14:54:38 -0300

    feat: add properties(pieces, players) & finish method in BaseGame

--- src/core/models/BaseGame.java
+++ src/core/models/BaseGame.java
@@ -2,16 +2,24 @@ package core.models;
 
 import core.enums.GameState;
 
+import java.util.List;
+
 public abstract class BaseGame {
     private GameState gameState;
     private final BaseBoard board;
+    private final List<BasePiece> pieces;
+    private final List<Player> players;
+    private Player currentPlayer;
 
-    public BaseGame(BaseBoard board) {
+    public BaseGame(BaseBoard board, List<BasePiece> pieces, List<Player> players) {
         this.board = board;
+        this.pieces = pieces;
+        this.players = players;
         this.gameState = GameState.NOT_STARTED;
     }
 
-    public abstract void initialize();
+    protected abstract void initialize();
+    protected abstract void finish();
 
     public void start(){
         if(this.gameState == GameState.NOT_STARTED){
@@ -30,5 +38,37 @@ public abstract class BaseGame {
         if(this.gameState == GameState.PAUSED){
             this.gameState = GameState.IN_PROGRESS;
         }
+    }
+
+    public void end(){
+        if(this.gameState == GameState.IN_PROGRESS){
+            finish();
+            this.gameState = GameState.FINISHED;
+        }
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+
+    public BaseBoard getBoard() {
+        return board;
+    }
+
+    public List<BasePiece> getPieces() {
+        return pieces;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public void setCurrentPlayer(Player currentPlayer) {
+        this.currentPlayer = currentPlayer;
     }
 }
